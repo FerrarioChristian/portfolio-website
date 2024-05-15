5b82d6b255af8583a288218a91e843babfc4e05a
FerrarioChristian <chri.fer.emi@gmail.com>
2024-05-15 22:22:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const trailer = document.getElementById("trailer");
+
+const animateTrailer = (e, interacting) => {
+  const x = e.clientX - trailer.offsetWidth / 2,
+    y = e.clientY - trailer.offsetHeight / 2;
+
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 1.5 : 1})`,
+  };
+
+  trailer.animate(keyframes, {
+    duration: 80,
+    fill: "forwards",
+  });
+};
+
+window.onmousemove = (e) => {
+  const interactable = e.target.closest(".interactable"),
+    interacting = interactable !== null;
+
+  const icon = document.getElementById("trailer-icon");
+
+  animateTrailer(e, interacting);
+
+  trailer.dataset.type = interacting ? interactable.dataset.type : "";
+
+  if (interacting) {
+    icon.className = "fa-solid fa-arrow-up-right";
+  }
+};
